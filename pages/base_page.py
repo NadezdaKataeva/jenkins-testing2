236a88b4f96f5823dc607fd20e1820c79a48e4f7
StanAretinskiy <sr575sr@gmail.com>
2021-12-21 21:15:33 -0800

    add methods to base_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,19 @@
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class BasePage:
 
     def __init__(self, driver):
         self.driver = driver
+        self.wait = None
+
+    def get_wait(self):
+        if self.wait:
+            return self.wait
+        self.wait = WebDriverWait(self.driver, 5)
+        return self.wait
 
     def get_title(self):
         return self.driver.title
@@ -14,3 +24,13 @@ class BasePage:
     def go_to_page(self, url: str):
         self.driver.get(url)
         return self
+
+    def is_visible(self, locator):
+        try:
+            self.get_wait().until(EC.visibility_of_element_located(locator))
+            return True
+        except TimeoutException:
+            return False
+
+    def get_text(self, locator):
+        return self.get_wait().until(EC.visibility_of_element_located(locator)).text
