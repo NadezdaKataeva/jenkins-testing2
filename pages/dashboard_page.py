1b8979716bddb0f0c7c5540843024b399e2efd51
StanAretinskiy <sr575sr@gmail.com>
2021-12-23 20:14:54 -0800

    update dashboard_page.py

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -7,16 +7,15 @@ class DashboardPage(BasePage):
 
     PAGE_TITLE = 'Dashboard [Jenkins]'
 
-    _new_item = By.XPATH, "//a[@title='New Item']"
-    _dashboard_menu_anchor = (By.CSS_SELECTOR, 'a.breadcrumbBarAnchor')
-    _menu_selector = (By.ID, 'menuSelector')
-    _right_arrow = (By.XPATH, '//ul[@id="breadcrumbs"]/li[@class="children"]')
-    _right_arrow_menu = (By.XPATH, '//a[@href="/view/all/"]')
-    _right_arrow_menu_icon = (By.XPATH, '//div[@id="breadcrumb-menu-target"]/div')
+    DASHBOARD_MENU_ANCHOR = (By.CSS_SELECTOR, 'a.breadcrumbBarAnchor')
+    MENU_SELECTOR = (By.ID, 'menuSelector')
+    RIGHT_ARROW = (By.XPATH, '//ul[@id="breadcrumbs"]/li[@class="children"]')
+    RIGHT_ARROW_MENU = (By.XPATH, '//a[@href="/view/all/"]')
+    RIGHT_ARROW_MENU_ICON = (By.XPATH, '//div[@id="breadcrumb-menu-target"]/div')
 
     def __init__(self, driver):
         super().__init__(driver)
-        self.go_to_page(TD.BASE_URL)
+        self.url = TD.BASE_URL
 
     def click_new_item(self):
-        self.driver.find_element(self._new_item[0], self._new_item[1]).click()
+        self.click((By.XPATH, '//a[@title="New Item"]'))
