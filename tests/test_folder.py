46eae986f8ffe8de0e25e8b54b24e95542cd3d7b
NadiaK <nadia.kataeva@gmail.com>
2022-01-01 23:16:25 -0800

    added test delete created folder

--- tests/test_folder.py
+++ tests/test_folder.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.common.by import By
+
 from pages.dashboard_page import DashboardPage
 from pages.folder_config import FolderConfig
 from pages.job_folder import JobFolder
@@ -6,22 +8,37 @@ from tests.base_test import BaseTest
 
 
 class TestFolder(BaseTest):
+    folder_name = 'testfolder120'
 
     def test_create_folder_valid_name(self):
-        folder_name = 'testfolder123'
 
         DashboardPage(self.driver).click_new_item()
 
         new_job = NewJob(self.driver)
-        new_job.enter_item_name(folder_name)
+        new_job.enter_item_name(self.folder_name)
         new_job.click_folder()
         assert new_job.is_ok_button_clickable()
         new_job.click_ok_button()
 
         folder_config = FolderConfig(self.driver)
-        assert folder_config.get_current_url().endswith(f'/job/{folder_name}/configure')
+        assert folder_config.get_current_url().endswith(f'/job/{self.folder_name}/configure')
         folder_config.click_save()
 
-        job_folder = JobFolder(self.driver)
-        assert job_folder.get_current_url().endswith(f'/job/{folder_name}/')
-        assert job_folder.get_page_title() == folder_name
+        job_folder = JobFolder(self.driver, self.folder_name)
+        assert job_folder.get_current_url().endswith(f'/job/{self.folder_name}/')
+        assert job_folder.get_page_title() == self.folder_name
+
+
+    def test_delete_created_folder(self):
+
+        job_folder = JobFolder(self.driver, self.folder_name)
+        job_folder.click_delete_folder()
+        assert job_folder.get_current_url() == DashboardPage.DASHBOARD_URL
+
+        dashboard =DashboardPage(self.driver)
+        table_jobs= dashboard.get_text(DashboardPage.DASHBOARD_TABLE)
+        assert self.folder_name not in table_jobs
+
+
+
+
