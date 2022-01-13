cc4caf76835fdb60318122455aaea19b870c5cb8
dsk1975 <dsko1975@gmail.com>
2022-01-12 17:42:32 -0800

    Add mechod to add and remove random folder Add package python-jenkins==1.7.0, take into account the comments

---
+++ tests/test_folder_random.py
@@ -0,0 +1,39 @@
+from pages.dashboard_page import DashboardPage
+from pages.folder_config import FolderConfig
+from pages.job_folder import JobFolder
+from pages.new_job import NewJob
+from tests.base_test import BaseTest
+from utils import jenkins_libs as jl
+import random
+
+
+class TestFolder(BaseTest):
+
+    jen_libs = jl.JenkinsLibs.jenkins_server
+
+    def test_create_folder_valid_name(self):
+        # folder_name = 'testfolder123'
+        seq = random.randint(100, 999)
+        folder_name = 'folder' + str(seq)
+
+
+        DashboardPage(self.driver).click_new_item()
+        # if dp.is_job_exist(folder_name):
+        #     self.jen_libs.delete_job(folder_name)
+        # dp.click_new_item()
+
+        new_job = NewJob(self.driver)
+        new_job.enter_item_name(folder_name)
+        new_job.click_folder()
+        assert new_job.is_ok_button_clickable()
+        new_job.click_ok_button()
+
+        folder_config = FolderConfig(self.driver)
+        assert folder_config.get_current_url().endswith(f'/job/{folder_name}/configure')
+        folder_config.click_save()
+
+        job_folder = JobFolder(self.driver, name=folder_name)
+        assert job_folder.get_current_url().endswith(f'/job/{folder_name}/')
+        assert job_folder.get_page_title() == folder_name
+
+        self.jen_libs.delete_job(folder_name)
