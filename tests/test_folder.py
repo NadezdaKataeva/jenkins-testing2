7b957495eae5ff250d9251e78a7853fec2c35aea
NadiaK <nadia.kataeva@gmail.com>
2022-02-24 19:21:45 -0800

    Changed xpath locator for Save button in new version of jenkins

--- tests/test_folder.py
+++ tests/test_folder.py
@@ -1,9 +1,3 @@
-
-from selenium.webdriver.common.by import By
-
-import pytest
-
-
 from pages.dashboard_page import DashboardPage
 from pages.folder_config import FolderConfig
 from pages.job_folder import JobFolder
@@ -13,12 +7,10 @@ from utils import jenkins_libs as jl
 
 
 class TestFolder(BaseTest):
-    folder_name = 'testfolder1180'
 
     jen_libs = jl.JenkinsLibs.jenkins_server
 
     def test_create_folder_valid_name(self):
-
         folder_name = 'testfolder123'
 
         dp = DashboardPage(self.driver)
@@ -26,18 +18,17 @@ class TestFolder(BaseTest):
             self.jen_libs.delete_job(folder_name)
         dp.click_new_item()
 
-
         new_job = NewJob(self.driver)
-        new_job.enter_item_name(self.folder_name)
+        new_job.enter_item_name(folder_name)
         new_job.click_folder()
         assert new_job.is_ok_button_clickable()
         new_job.click_ok_button()
 
         folder_config = FolderConfig(self.driver)
-        assert folder_config.get_current_url().endswith(f'/job/{self.folder_name}/configure')
+        assert folder_config.get_current_url().endswith(f'/job/{folder_name}/configure')
         folder_config.click_save()
 
-        job_folder = JobFolder(self.driver, self.folder_name)
+        job_folder = JobFolder(self.driver)
         assert job_folder.get_current_url().endswith(f'/job/{folder_name}/')
         assert job_folder.get_page_title() == folder_name
 
